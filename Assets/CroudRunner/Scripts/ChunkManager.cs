478baa17d12b1d3c49c20a53220f118b218a4ef4
John Klaine Jr <56973447+ScytheDragonStudios@users.noreply.github.com>
2023-03-30 08:10:38 -0400

    Commit 6: Added Chunk Manager Script. Set up Random road chunk generator. Fixed positions

---
+++ Assets/CroudRunner/Scripts/ChunkManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ChunkManager : MonoBehaviour
+{
+    [Header(" Header ")]
+    [SerializeField] private Chunk[] chunksPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 chunkPosition = Vector3.zero;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Chunk chunkToCreate = chunksPrefabs[Random.Range(0, chunksPrefabs.Length)];
+
+            if (i > 0)
+            {
+                chunkPosition.z += chunkToCreate.GetLength() / 2;
+            }
+
+            Chunk chunkInstance = Instantiate(chunkToCreate, chunkPosition, Quaternion.identity, transform);
+
+            chunkPosition.z += chunkInstance.GetLength() / 2;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
